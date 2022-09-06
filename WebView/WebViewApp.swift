db108641f03479fe255c7feaaceab3bbbf4469d7
Sakdinan Sukkhasem <sakdinan.suk@gmail.com>
2022-09-06 11:56:09 +0700

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Sakdinan Sukkhasem on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
